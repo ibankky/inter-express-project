d1bc20f2ba6627c7634b5271291570b30dcb91ef
nattapong narkbanpot <nattapong_nar@truecorp.co.th>
2022-09-07 19:06:29 +0700

    init project

---
+++ pages/concept.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+<script>
+  import { ref} from 'vue'
+  export default {
+    setup() {
+      const message = ref('INTER EXPRESS Vue.js')
+      return {
+        message
+      }
+    }
+  }
+</script>
