e292a0c4d37b92179483b2059027921ac089ace0
nattapong narkbanpot <ibankky2011@gmail.com>
2022-10-17 09:27:59 +0700

    fix product add flow

--- components/share/MobileMenu.vue
+++ components/share/MobileMenu.vue
@@ -3,37 +3,37 @@
     <div class="flex w-100 py-5 px-1 text-[12px]">
       <div class="w-1/6 text-center justify-center">
         <div class="flex text-center justify-center">
-          <img src="icons/product-icon.svg" />
+          <img src="/icons/product-icon.svg"  class="w-[28px] h-[28px]"/>
         </div>
         <div class="mt-1">หน้าหลัก</div>
       </div>
      <div class="w-1/6 text-center justify-center">
         <div class="flex text-center justify-center">
-          <img src="icons/product-icon.svg" />
+          <img src="/icons/box.svg" class="w-[28px] h-[28px]"/>
         </div>
         <div class="mt-1">สินค้า</div>
       </div>
      <div class="w-1/6 text-center justify-center">
         <div class="flex text-center justify-center">
-          <img src="icons/product-icon.svg" />
+          <img src="/icons/megaphone.svg" class="w-[28px] h-[28px]"/>
         </div>
         <div class="mt-1">โฆษณา</div>
       </div>
       <div class="w-1/6 text-center justify-center">
         <div class="flex text-center justify-center">
-          <img src="icons/product-icon.svg" />
+          <img src="/icons/vuesax-linear-messages.svg" class="w-[28px] h-[28px]"/>
         </div>
         <div class="mt-1">ข้อความ</div>
       </div>
       <div class="w-1/6 text-center justify-center">
         <div class="flex text-center justify-center">
-          <img src="icons/product-icon.svg" />
+          <img src="/icons/vuesax-linear-notification.svg" class="w-[28px] h-[28px]"/>
         </div>
         <div class="mt-1">แจ้งเตือน</div>
       </div>
       <div class="w-1/6 text-center justify-center">
         <div class="flex text-center justify-center">
-          <img src="icons/product-icon.svg" />
+          <img src="/icons/product-icon.svg"  class="w-[28px] h-[28px]"/>
         </div>
         <div class="mt-1">เมนู</div>
       </div>
